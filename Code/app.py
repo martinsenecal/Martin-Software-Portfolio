4e997d9faa873ed1efe851ffa42b98ce6ea409ff
Martin Senécal <martinsenecal2000@gmail.com>
2020-02-25 23:06:59 -0500

    Initial commit

---
+++ Code/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/')
+def homepage():
+    return render_template("home.html")
+
+
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
+
+
+@app.route('/education')
+def education():
+    return render_template("education.html")
+
+
+
+if __name__ == '__main__':
+    app.run()
